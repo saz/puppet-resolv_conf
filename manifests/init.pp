409a2282e19d5a88631e4daed6753e448fb79a2f
Saz <me@saz.sh>
2011-08-01 11:49:09 +0200

    Use dnsmasq::conf instead of dnsmasq

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,10 @@
-class resolv_conf($domain = $domain, $searchpath, $nameservers, $use_dnsmasq = false) {
+class resolv_conf($domainname = $domain, $searchpath, $nameservers, $use_dnsmasq = false) {
     include resolv_conf::params, resolv_conf::config
 
     if $use_dnsmasq {
-        class { 'dnsmasq':
-            domain      => $domain,
-            searchpath  => $searchpath,
-            nameservers => $nameservers,
+        dnsmasq::conf { 'local-dns':
+            ensure => present,
+            source => 'puppet:///modules/resolv_conf/dnsmasq_local-dns',
         }
     }
 }
