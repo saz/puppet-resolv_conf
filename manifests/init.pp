57580b416e5e02535f1595e044f4031a0100a6ac
Steffen Zieger <me@saz.sh>
2012-11-07 00:27:11 +0100

    move config to init class, fix searchpath and options

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,14 +4,27 @@ class resolv_conf(
   $domainname = $domain,
   $options = undef,
   $use_dnsmasq = false
-) {
-  include resolv_conf::params
-  include resolv_conf::config
+) inherits resolv_conf::params {
 
   if $use_dnsmasq {
     dnsmasq::conf { 'local-dns':
       ensure => present,
       source => 'puppet:///modules/resolv_conf/dnsmasq_local-dns',
     }
+  }
+
+  $require_real = $use_dnsmasq ? {
+    true    => Dnsmasq::Conf['local-dns'],
+    default => undef,
+  }
+
+  file { 'resolv.conf':
+    ensure  => file,
+    path    => $resolv_conf::params::config_file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('resolv_conf/resolv.conf.erb'),
+    require => $require_real,
   }
 }
