2fa35d90ca291ef8b84cdda8d57d87caf52d1fdf
Donald Clark Jackson <dcj@clark-communications.com>
2014-06-02 14:18:39 -0700

    give up, new direction

---
+++ manifests/create.pp
@@ -0,0 +1,28 @@
+define resolv_conf::create (
+  $searchpath,
+  $nameservers,
+  $domainname = $::domain,
+  $options = undef,
+  $config_file = $::resolv_conf::params::config_file,
+  $owner = 'root',
+  $group = $::resolv_conf::params::group,
+  $mode  = '0644'
+) {
+
+  include resolv_conf
+
+  # if $config_file {
+  #   $resolv_conf_file = $config_file
+  #   } else {
+  #   $resolv_conf_file = $resolv_conf::params::config_file
+  # }
+
+  file { "resolv.conf.$name":
+    ensure  => file,
+    path    => $config_file,
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    content => template('resolv_conf/resolv.conf.erb'),
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,5 @@
-class { '::resolv_conf::params': } ->
-
-define resolv_conf (
-  $searchpath,
-  $nameservers,
-  $domainname = $::domain,
-  $options = undef,
-  $config_file = $::resolv_conf::params::config_file,
-  $owner = 'root',
-  $group = $::resolv_conf::params::group,
-  $mode  = '0644'
-) {
-
-  # if $config_file {
-  #   $resolv_conf_file = $config_file
-  #   } else {
-  #   $resolv_conf_file = $resolv_conf::params::config_file
-  # }
-
-  file { "resolv.conf.$name":
-    ensure  => file,
-    path    => $config_file,
-    owner   => $owner,
-    group   => $group,
-    mode    => $mode,
-    content => template('resolv_conf/resolv.conf.erb'),
-  }
+class resolv_conf {
+  class { '::resolv_conf::params' }
 }
+
 
