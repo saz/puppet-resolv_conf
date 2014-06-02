4191e603b9fbb48e481886f07073ec565ee16032
Donald Clark Jackson <dcj@clark-communications.com>
2014-06-02 11:30:59 -0700

    add (optional) param config_file

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,19 @@ class resolv_conf(
   $searchpath,
   $nameservers,
   $domainname = $::domain,
-  $options = undef
+  $options = undef,
+  $config_file = undef
 ) inherits resolv_conf::params {
+
+  if $config_file {
+    $resolv_conf_file = $config_file
+    } else {
+    $resolv_conf_file = $resolv_conf::params::config_file
+  }
 
   file { 'resolv.conf':
     ensure  => file,
-    path    => $resolv_conf::params::config_file,
+    path    => $resolv_conf_file,
     owner   => 'root',
     group   => $resolv_conf::params::group,
     mode    => '0644',
