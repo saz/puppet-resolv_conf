52228bc6c367600e963661c7ba070349f742d495
Zan Loy <zan.loy@gmail.com>
2014-12-17 09:10:17 -0500

    initial solaris add

---
+++ manifests/solaris.pp
@@ -0,0 +1,24 @@
+class resolv_conf::solaris (
+  $domainname,
+  $searchpath,
+  $nameservers,
+  $options,
+) {
+
+  svccfg { 'resolv-nameserver':
+    ensure => present,
+    fmri => 'svc:/network/dns/client,
+    property => 'config/nameserver',
+    type => 'array',
+    value => $nameservers,
+  }
+
+  svccfg { 'resolv-searchpath':
+    ensure => present,
+    fmri => 'svc:/network/dns/client,
+    property => 'config/search',
+    type => 'string',
+    value => $searchpath,
+  }
+
+}
