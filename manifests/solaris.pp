4405363d3679c9a5faea569f14f415698f4e1e8d
Zan Loy <zan.loy@gmail.com>
2014-12-17 09:51:03 -0500

    quotes!

--- manifests/solaris.pp
+++ manifests/solaris.pp
@@ -7,14 +7,14 @@ class resolv_conf::solaris (
 
   $nameservers_string = join($nameservers, ' ')
 
-  exec { "/usr/sbin/svccfg -s dns/client setprop config/nameserver = net_address: \"(${nameservers_string})\"":
-    unless => "/usr/sbin/svccfg -s dns/client listprop config/nameserver | grep \"${nameservers_string}\"",
+  exec { "/usr/sbin/svccfg -s dns/client setprop config/nameserver = net_address: '(${nameservers_string})'":
+    unless => "/usr/sbin/svccfg -s dns/client listprop config/nameserver | grep '${nameservers_string}'",
   }
     
   $search_string = join(any2array($searchpath), ' ')
 
-  exec { "/usr/sbin/svccfg -s dns/client setprop config/search = astring: \"(${search_string})\"":
-    unless => "/usr/sbin/svccfg -s dns/client listprop config/search | grep \"${search_string}\"",
+  exec { "/usr/sbin/svccfg -s dns/client setprop config/search = astring: '(\"${search_string}\")'":
+    unless => "/usr/sbin/svccfg -s dns/client listprop config/search | grep '${search_string}'",
   }
 
 }
