65df29ee8d3c733f589f07ea71d59c1bce1f0e6c
Zan Loy <zan.loy@gmail.com>
2014-12-17 09:38:59 -0500

    any2array

--- manifests/solaris.pp
+++ manifests/solaris.pp
@@ -5,7 +5,7 @@ class resolv_conf::solaris (
   $options,
 ) {
 
-  $nameserver_string = join($nameserver, ' ')
+  $nameserver_string = join(any2array($nameserver), ' ')
 
   exec { "/usr/sbin/svccfg -s dns/client setprop config/nameserver = net_address: \"(${nameserver_string})\"":
     unless => "/usr/sbin/svccfg -s dns/client listprop config/nameserver | grep \"${nameserver_string}\"",
