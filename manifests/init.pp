e2ef3fc4b02cc21eba5902833212d7849cad2a38
Steffen Zieger <me@saz.sh>
2017-01-06 21:36:53 +0100

    improve solaris support, fixes #33

--- manifests/init.pp
+++ manifests/init.pp
@@ -28,23 +28,24 @@ class resolv_conf(
   } elsif $domainname != undef and $searchpath == [] {
     $domainname_real = $domainname
   } elsif $domainname != undef and $searchpath != [] {
-    fail('domainname and searchpath are mutually exclusive parameters')
+    if $::osfamily != 'Solaris' {
+      fail('domainname and searchpath are mutually exclusive parameters')
+    }
+  }
+
+  file { $config_file:
+    ensure  => file,
+    owner   => 'root',
+    group   => 0,
+    mode    => '0644',
+    content => template('resolv_conf/resolv.conf.erb'),
   }
 
   if $::osfamily == 'Solaris' and $::operatingsystemmajrelease == '11' {
-    class { '::resolv_conf::solaris':
-      domainname  => $domainname_real,
-      searchpath  => $searchpath,
-      nameservers => $nameservers,
-      options     => $options,
-    }
-  } else {
-    file { $config_file:
-      ensure  => file,
-      owner   => 'root',
-      group   => 0,
-      mode    => '0644',
-      content => template('resolv_conf/resolv.conf.erb'),
+    exec { 'load resolv.conf in smf':
+      command     => '/usr/sbin/nscfg import -f dns/client',
+      refreshonly => true,
+      subscribe   => File[$resolv_conf::params::config_file],
     }
   }
 }
